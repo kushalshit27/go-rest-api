0570ddfcf13a10928ac7d95036a5885a40ad58fb
kushal_s <kushalshit27@gmail.com>
2021-04-24 11:21:15 +0530

    sql update

--- database.sql
+++ database.sql
@@ -1,9 +1,43 @@
-CREATE TABLE posts (
-	id serial PRIMARY KEY,
-	title VARCHAR(255) NOT NULL,
-	description TEXT DEFAULT '',
-	created_on TIMESTAMP  DEFAULT NULL,
-	status BOOLEAN NOT NULL DEFAULT 'true'
+BEGIN;
+
+
+CREATE TABLE public.blogs
+(
+    title character varying(255) NOT NULL,
+    description text NOT NULL,
+    status boolean NOT NULL,
+    created_at timestamp(0) without time zone NOT NULL,
+    updated_at timestamp(0) without time zone NOT NULL,
+    deleted_at timestamp(0) without time zone,
+    created_by bigint NOT NULL,
+    id integer NOT NULL,
+    PRIMARY KEY (id)
 );
 
+CREATE TABLE public.passwords
+(
+    id bigint NOT NULL,
+    password character varying(255) NOT NULL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE public.users
+(
+    name character(255) NOT NULL,
+    email character(255) NOT NULL,
+    age integer NOT NULL,
+    role character varying NOT NULL,
+    created_at timestamp(0) without time zone NOT NULL,
+    updated_at timestamp(0) without time zone NOT NULL,
+    deleted_at timestamp(0) without time zone,
+    status boolean NOT NULL,
+    id integer NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE public.blogs
+    ADD FOREIGN KEY (created_by)
+    REFERENCES public.users (id)
+    NOT VALID;
+
+END;
